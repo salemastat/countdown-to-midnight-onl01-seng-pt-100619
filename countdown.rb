074a57219d44d418858e9b213a77718c8a7fd3b1
Salem Asrat <salemasrat@gmail.com>
2019-10-12 01:56:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,17 +6,4 @@ puts "#{number} SECOND(S)!"
 number -= 1
  end
  puts "HAPPY NEW YEAR!"
-end 
-
-
-def countdown_with_sleep(num)
-
-while num > 0
-
-  puts "#{num} SECOND(S)!"
-  sleep(1)
-  num -= 1
-
-end
-  "HAPPY NEW YEAR!"
-end	
+e 

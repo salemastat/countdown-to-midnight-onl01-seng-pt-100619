9c08649a5fba516c271690c8d0245b91dc2cab92
Salem Asrat <salemasrat@gmail.com>
2019-10-12 01:57:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,18 @@
 #write your code here
 
-def countdown(num)
-while num > 0
-puts "#{number} SECOND(S)!"
-number -= 1
- end
- puts "HAPPY NEW YEAR!"
-e 
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
